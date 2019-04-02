c99fbb2e96323ecc7289f59da87eee18a323fc52
durgarao <asapudurgarao@gmail.com>
2019-04-02 22:10:51 +0530

    heroku deploy updates

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-//Install express server
+// server.js
 const express = require('express');
-const path = require('path');
-
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/detect-quiz'));
-
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/detect-quiz/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
 app.listen(process.env.PORT || 8080);
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'quiz', pathMatch: 'full' },
+  {
+    path: 'quiz',
+    // component: SpuredComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
